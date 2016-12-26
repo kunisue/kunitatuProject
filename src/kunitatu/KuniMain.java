c38323114e4985854dcbe35d5f05dd738fed88c1
kunisue <kunitatu777@gmail.com>
2016-12-26 16:46:50 +0900

    sub 3rd commit.

--- src/kunitatu/KuniMain.java
+++ src/kunitatu/KuniMain.java
@@ -8,23 +8,27 @@ public class KuniMain {
 		System.exit(0);
 	}
 
-	// private static final String a = "a";
-	// private static final String b = "b";
-	// private static final String c = "c";
-	// private static final String d = "d";
-	// private static final String e = "e";
-	private String a = "a";
-	private String b = "b";
-	private String c = "c";
-	private String d = "d";
-	private String e = "e";
+	private static final String a = "a";
+	private static final String b = "b";
+	private static final String c = "c";
+	private static final String d = "d";
+	private static final String e = "e";
+//	private String a = "a";
+//	private String b = "b";
+//	private String c = "c";
+//	private String d = "d";
+//	private String e = "e";
 
+	// ï∂éöóÒåãçáë¨ìxí≤ç∏
 	private void test01() {
 		int max = 300000;
 
 		this.doStringPlus(max);
 		this.doStringJoin(max);
 		this.doStringBuilder(max);
+		this.doStringBuilder(max);
+		this.doStringJoin(max);
+		this.doStringPlus(max);
 	}
 
 	private void doStringBuilder(int max) {
@@ -33,7 +37,7 @@ public class KuniMain {
 		long time = System.currentTimeMillis();
 		for (int i = 0; i < max; i++) {
 			sb = new StringBuilder();
-			sb.append(a).append(b).append(c).append(d).append(e).append("f").append(String.valueOf(i));
+			sb.append(a).append(b).append(c).append(d).append(e).append("f").append(String.valueOf(i + max * 2));
 			str = sb.toString();
 		}
 		log("doStringBuilder:" + (System.currentTimeMillis() - time));
@@ -44,7 +48,7 @@ public class KuniMain {
 		String str = "";
 		long time = System.currentTimeMillis();
 		for (int i = 0; i < max; i++) {
-			str = String.join(a, b, c, d, e, "f", String.valueOf(i));
+			str = String.join(a, b, c, d, e, "g", String.valueOf(i + max * 3));
 		}
 		log("doStringJoin:" + (System.currentTimeMillis() - time));
 
@@ -54,7 +58,7 @@ public class KuniMain {
 		String str = "";
 		long time = System.currentTimeMillis();
 		for (int i = 0; i < max; i++) {
-			str = a + b + c + d + e + "f" + String.valueOf(i);
+			str = a + b + c + d + e + "h" + String.valueOf(i + max * 4);
 		}
 		log("doStringPlus:" + (System.currentTimeMillis() - time));
 
